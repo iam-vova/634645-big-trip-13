5aabf3c2d05a8a0c0d7ee67ecdcc4e09a3ea6ad1
brylev.v <iam-vova@yandex.ru>
2020-11-18 00:19:11 +0300

    1.6 Разбиваем код на модули

--- src/main.js
+++ src/main.js
@@ -1,3 +1,50 @@
+import {createTripInfoTepmplate} from "./view/trip-info";
+import {createTripCostTemplate} from "./view/trip-cost";
+import {createSiteMenuTemplate} from "./view/site-nav";
+import {createSiteFiltersTemplate} from "./view/site-filters";
+import {createTripSortTemplate} from "./view/trip-sort";
+import {createEventsListTemplate} from "./view/events-list";
+import {createEventsItemTemplate} from "./view/events-item";
+import {createEventsCreateFormTemplate} from "./view/events-create-form";
+import {createEventsEditFormTemplate} from "./view/events-edit-form";
+
+const EVENTS_COUNT = 3;
+
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
+
+const siteHeaderElement = document.querySelector(`.page-header`);
+const siteMainElement = document.querySelector(`.page-main`);
+const siteTripMainElement = siteHeaderElement.querySelector(`.trip-main`);
+
+render(siteTripMainElement, createTripInfoTepmplate(), `afterbegin`);
+
+const siteTripInfoElement = siteTripMainElement.querySelector(`.trip-info`);
+render(siteTripInfoElement, createTripCostTemplate(), `beforeend`);
+
+const siteControlsElement = siteTripMainElement.querySelector(`.trip-controls`);
+render(siteControlsElement, createSiteMenuTemplate(), `afterbegin`);
+render(siteControlsElement, createSiteFiltersTemplate(), `beforeend`);
+
+const siteEventsElement = siteMainElement.querySelector(`.trip-events`);
+render(siteEventsElement, createTripSortTemplate(), `beforeend`);
+render(siteEventsElement, createEventsListTemplate(), `beforeend`);
+
+const siteEventsListElement = siteEventsElement.querySelector(`.trip-events__list`);
+
+// render(siteEventsListElement, createEventsCreateFormTemplate(), 'afterbegin');
+render(siteEventsListElement, createEventsEditFormTemplate(), `afterbegin`);
+
+for (let i = 0; i < EVENTS_COUNT; i++) {
+  render(siteEventsListElement, createEventsItemTemplate(), `beforeend`);
+}
+
+// Информация о маршруте;
+// Стоимость поездки;
+// Меню;
+// Фильтры;
+// Сортировка;
+// Форма создания;
+// Форма редактирования;
+// Точка маршрута (в списке)
