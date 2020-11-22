63682fac669c21e5a27c7c2483ce198d8e519455
brylev.v <iam-vova@yandex.ru>
2020-11-22 21:19:03 +0300

    2.1 Описываем структуру данных задачи

---
+++ src/mock/point.js
@@ -0,0 +1,38 @@
+// Функция из интернета по генерации случайного числа из диапазона
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
+const getRandomInteger = (a = 0, b = 1) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+
+  return Math.floor(lower + Math.random() * (upper - lower + 1));
+};
+
+const generatePointType = () => {
+  const types = [
+    `Taxi`,
+    `Bus`,
+    `Train`,
+    `Ship`,
+    `Transport`,
+    `Drive`,
+    `Flight`,
+    `Check-in`,
+    `Sightseeing`,
+    `Restaurant`
+  ];
+
+  const randomIndex = getRandomInteger(0, types.length - 1);
+
+  return types[randomIndex];
+}
+
+export const generatePoint = () => {
+  return {
+    type: generatePointType(),
+    destination: null,
+    dateFrom: null,
+    dateTo: null,
+    cost: null
+    isFavorite: false
+  };
+};
