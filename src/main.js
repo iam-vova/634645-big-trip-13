9bd1d408314a118ba1fb815d7db2e018e5e7018f
brylev.v <iam-vova@yandex.ru>
2020-12-02 22:37:50 +0300

    3.1.3 Переименуем renderElement в render

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import EventsItem from "./view/events-item";
 // import EventsCreateForm from "./view/events-create-form";
 import EventsEditForm from "./view/events-edit-form";
 import {generatePoint} from "./mock/point";
-import {renderTemplate, renderElement, RenderPosition} from "./utils/common";
+import {renderTemplate, render, RenderPosition} from "./utils/common";
 
 const EVENTS_COUNT = 5;
 const events = new Array(EVENTS_COUNT).fill().map(generatePoint);
@@ -17,26 +17,26 @@ const siteHeaderElement = document.querySelector(`.page-header`);
 const siteMainElement = document.querySelector(`.page-main`);
 const siteTripMainElement = siteHeaderElement.querySelector(`.trip-main`);
 
-renderElement(siteTripMainElement, new TripInfo(events).getElement(), RenderPosition.AFTERBEGIN);
+render(siteTripMainElement, new TripInfo(events).getElement(), RenderPosition.AFTERBEGIN);
 
 const siteTripInfoElement = siteTripMainElement.querySelector(`.trip-info`);
-renderElement(siteTripInfoElement, new TripCost(events).getElement(), RenderPosition.BEFOREEND);
+render(siteTripInfoElement, new TripCost(events).getElement(), RenderPosition.BEFOREEND);
 
 const siteControlsElement = siteTripMainElement.querySelector(`.trip-controls`);
-renderElement(siteControlsElement, new SiteMenu().getElement(), RenderPosition.AFTERBEGIN);
-renderElement(siteControlsElement, new SiteFilters().getElement(), RenderPosition.BEFOREEND);
+render(siteControlsElement, new SiteMenu().getElement(), RenderPosition.AFTERBEGIN);
+render(siteControlsElement, new SiteFilters().getElement(), RenderPosition.BEFOREEND);
 
 const siteEventsElement = siteMainElement.querySelector(`.trip-events`);
-renderElement(siteEventsElement, new TripSort().getElement(), RenderPosition.BEFOREEND);
-renderElement(siteEventsElement, new EventsList().getElement(), RenderPosition.BEFOREEND);
+render(siteEventsElement, new TripSort().getElement(), RenderPosition.BEFOREEND);
+render(siteEventsElement, new EventsList().getElement(), RenderPosition.BEFOREEND);
 
 const siteEventsListElement = siteEventsElement.querySelector(`.trip-events__list`);
 
-// renderElement(siteEventsListElement, new EventsCreateForm(events[0]).getElement(), RenderPosition.AFTERBEGIN);
-renderElement(siteEventsListElement, new EventsEditForm(events[0]).getElement(), RenderPosition.AFTERBEGIN);
+// render(siteEventsListElement, new EventsCreateForm(events[0]).getElement(), RenderPosition.AFTERBEGIN);
+render(siteEventsListElement, new EventsEditForm(events[0]).getElement(), RenderPosition.AFTERBEGIN);
 
 for (let i = 0; i < EVENTS_COUNT; i++) {
-  renderElement(siteEventsListElement, new EventsItem(events[i]).getElement(), RenderPosition.BEFOREEND);
+  render(siteEventsListElement, new EventsItem(events[i]).getElement(), RenderPosition.BEFOREEND);
 }
 
 // Информация о маршруте;
