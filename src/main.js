2f2f1616c4a4e8b44eb6c8291de067fb7fbf2977
brylev.v <iam-vova@yandex.ru>
2020-12-02 21:55:13 +0300

    3.1.1 Рефакторинг рендер-метода

--- src/main.js
+++ src/main.js
@@ -8,38 +8,36 @@ import {createEventsItemTemplate} from "./view/events-item";
 // import {createEventsCreateFormTemplate} from "./view/events-create-form";
 import {createEventsEditFormTemplate} from "./view/events-edit-form";
 import {generatePoint} from "./mock/point";
+import {renderTemplate} from "./utils/common";
 
 const EVENTS_COUNT = 5;
 const events = new Array(EVENTS_COUNT).fill().map(generatePoint);
 
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
-};
 
 const siteHeaderElement = document.querySelector(`.page-header`);
 const siteMainElement = document.querySelector(`.page-main`);
 const siteTripMainElement = siteHeaderElement.querySelector(`.trip-main`);
 
-render(siteTripMainElement, createTripInfoTepmplate(events), `afterbegin`);
+renderTemplate(siteTripMainElement, createTripInfoTepmplate(events), `afterbegin`);
 
 const siteTripInfoElement = siteTripMainElement.querySelector(`.trip-info`);
-render(siteTripInfoElement, createTripCostTemplate(events), `beforeend`);
+renderTemplate(siteTripInfoElement, createTripCostTemplate(events), `beforeend`);
 
 const siteControlsElement = siteTripMainElement.querySelector(`.trip-controls`);
-render(siteControlsElement, createSiteMenuTemplate(), `afterbegin`);
-render(siteControlsElement, createSiteFiltersTemplate(), `beforeend`);
+renderTemplate(siteControlsElement, createSiteMenuTemplate(), `afterbegin`);
+renderTemplate(siteControlsElement, createSiteFiltersTemplate(), `beforeend`);
 
 const siteEventsElement = siteMainElement.querySelector(`.trip-events`);
-render(siteEventsElement, createTripSortTemplate(), `beforeend`);
-render(siteEventsElement, createEventsListTemplate(), `beforeend`);
+renderTemplate(siteEventsElement, createTripSortTemplate(), `beforeend`);
+renderTemplate(siteEventsElement, createEventsListTemplate(), `beforeend`);
 
 const siteEventsListElement = siteEventsElement.querySelector(`.trip-events__list`);
 
 // render(siteEventsListElement, createEventsCreateFormTemplate(events[0]), 'afterbegin');
-render(siteEventsListElement, createEventsEditFormTemplate(events[0]), `afterbegin`);
+renderTemplate(siteEventsListElement, createEventsEditFormTemplate(events[0]), `afterbegin`);
 
 for (let i = 0; i < EVENTS_COUNT; i++) {
-  render(siteEventsListElement, createEventsItemTemplate(events[i]), `beforeend`);
+  renderTemplate(siteEventsListElement, createEventsItemTemplate(events[i]), `beforeend`);
 }
 
 // Информация о маршруте;
