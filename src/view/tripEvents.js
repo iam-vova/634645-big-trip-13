5f85205cf258b1e118fbb378a069ac61c9ae2904
brylev.v <iam-vova@yandex.ru>
2020-12-10 22:17:55 +0300

    5.1.1 Добавляет два компонента

---
+++ src/view/tripEvents.js
@@ -0,0 +1,14 @@
+import AbstractEvent from "./abstractEvent";
+
+const createTripEventsTemplate = () => {
+  return `<section class="trip-events">
+            <h2 class="visually-hidden">Trip events</h2>
+
+          </section>`;
+}
+
+export default class TripEvents extends AbstractEvent {
+  getTemplate() {
+    return createTripEventsTemplate();
+  }
+}
